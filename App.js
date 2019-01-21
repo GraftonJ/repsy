457d851e2aa246236e98c8026271cd6055359361
Tim Remington <Tim.Remington29@gmail.com>
2019-01-21 12:51:41 -0700

    Added some navigation with homepage

--- App.js
+++ App.js
@@ -5,9 +5,10 @@ import { Router, Scene } from 'react-native-router-flux';
 import { Container, Header, Content, Footer } from 'native-base'
 
 import Homepage from './components/pages/homepage'
+import Loginpage from './components/pages/loginpage'
 
 // type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
 
   constructor(props){
     super(props)
@@ -19,7 +20,8 @@ export default class App extends Component<Props> {
     return (
       <Router>
         <Scene key="root" hideNavBar= "false">
-          <Scene key="homePage" component={Homepage} loginStatus={this.state.loginStatus} title="X Home" initial={true} />
+          <Scene key="Homepage" component={Homepage} />
+          <Scene key="Loginpage" component={Loginpage} initial={true} />
         </Scene>
       </Router>
     )
