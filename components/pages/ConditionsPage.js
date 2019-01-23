f2fcf5e97f1d951d815bb6935930eed5692c7b0a
Tim Remington <Tim.Remington29@gmail.com>
2019-01-23 10:14:05 -0700

    Change the name of footermenu to FooterMenu

--- components/pages/ConditionsPage.js
+++ components/pages/ConditionsPage.js
@@ -1,12 +1,35 @@
-// import React, { Component } from 'react';
-// import { Actions } from 'react-native-router-flux';
-// import { FooterTab, Button, Icon, Text } from 'native-base'
-//
-// export default class ConditionsPage extends Component {
-//
-//   render() {
-//     return (
-//
-//     )
-//   }
-// }
+import React, { Component } from 'react';
+import {Platform, StyleSheet, Image, View, Dimensions, AsyncStorage, FlatList} from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import { Container, Header, Content, Footer, Button, Left, Right, Body } from 'native-base'
+
+export default class ConditionsPage extends Component {
+
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Left>
+          </Left>
+          <Body>
+          </Body>
+          <Right>
+          </Right>
+        </Header>
+        <Content>
+        </Content>
+        <Footer>
+        </Footer>
+      </Container>
+    )
+  }
+}
+
+// Variables to changes the height and width dynamically for all screens
+const height = Dimensions.get('window').height
+const width = Dimensions.get('window').width
+
+// Put styles in here to format the page
+const styles = StyleSheet.create({
+
+});
