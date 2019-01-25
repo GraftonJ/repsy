5e7e25eed27cf6a679a62fd9faf19e4739c72368
Josh <grafton.josh@gmail.com>
2019-01-25 15:32:57 -0700

    working for JG

--- android/app/src/main/java/com/repsy/MainApplication.java
+++ android/app/src/main/java/com/repsy/MainApplication.java
@@ -3,7 +3,6 @@ package com.repsy;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -23,8 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNCWebViewPackage()
+          new MainReactPackage()
       );
     }
 
