ad9c4dbd29960205f2f378e82b921de64109c0e9
TaylorArmstrong <TArmstrong1327@gmail.com>
2019-01-27 22:30:24 -0700

    working

--- components/pages/RequestsPage.js
+++ components/pages/RequestsPage.js
@@ -7,8 +7,6 @@ import { Calendar, CalendarList, Agenda, Arrow } from 'react-native-calendars'
 
 import store, { URI } from '../../store'
 
-
-// import { google } from 'googleapis'
 // import { LocaleConfig } from 'react-native-calendars';
 
 // LocaleConfig.locales['fr'] = {
@@ -58,30 +56,6 @@ export default class RequestsPage extends Component {
     //     //   selected: day.dateString
     //     // })
     //   }
-    //Load Day Items for Agenda View
-    // function listEvents(auth) {
-    //   const calendar = google.calendar({ version: 'v3', auth });
-    //   calendar.events.list({
-    //     calendarId: 'primary',
-    //     timeMin: (new Date()).toISOString(),
-    //     maxResults: 10,
-    //     singleEvents: true,
-    //     orderBy: 'startTime',
-    //   }, (err, res) => {
-    //     if (err) return console.log('The API returned an error: ' + err);
-    //     const events = res.data.items;
-    //     if (events.length) {
-    //       console.log('Upcoming 10 events:');
-    //       events.map((event, i) => {
-    //         const start = event.start.dateTime || event.start.date;
-    //         console.log(`${start} - ${event.summary}`);
-    //       });
-    //     } else {
-    //       console.log('No upcoming events found.');
-    //     }
-    //   });
-    // }
-
     return (
       <Container>
         <Header>
