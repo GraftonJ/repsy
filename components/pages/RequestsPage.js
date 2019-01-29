c029502ca86c11cfa9446c5128f2a5cf268aa462
TaylorArmstrong <TArmstrong1327@gmail.com>
2019-01-28 21:01:53 -0700

    working

--- components/pages/RequestsPage.js
+++ components/pages/RequestsPage.js
@@ -25,7 +25,6 @@ export default class RequestsPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: {},
       doctorsAppointments: store.getState().doctorsAppointments,
       userID: store.getState().user.id,
       isLookingForAppointment: false,
@@ -42,7 +41,7 @@ export default class RequestsPage extends Component {
     //Get the conditions from the doctors_conditions route
     let appointments = []
     appointments = await getBookings()
-    console.log('hello')
+    console.log('appointments', appointments)
     //Set the store state with the conditions. This should cause local state to update and re-render
     store.setState({
       doctorsAppointment: appointments,
@@ -51,7 +50,6 @@ export default class RequestsPage extends Component {
 
   //Request new Appointment function for create request button
   requestAppointment = () => {
-    console.log('hello')
     this.setState({
       isLookingForAppointment: true,
     })
@@ -63,21 +61,12 @@ export default class RequestsPage extends Component {
   }
 
   render() {
-    // // Day Select for Month Calendar
-    // onDayPress = (day) => {
-
-    //     console.log('selected day', day)
-
-    //     // this.setState({
-    //     //   selected: day.dateString
-    //     // })
-    //   }
     return (
       <Container>
         <Header>
           <Left>
             <Button
-              onPress={() => {Actions.Homepage()}} title='Home Page'>
+              onPress={() => { Actions.Homepage() }} title='Home Page'>
             </Button>
           </Left>
           <Body>
@@ -86,61 +75,60 @@ export default class RequestsPage extends Component {
           <Right>
           </Right>
         </Header>
-          <Agenda
-            items={this.state.items}
-            loadItemsForMonth={this.loadItems.bind(this)}
-            selected={currentDate}
-            renderItem={this.renderItem.bind(this)}
-            renderEmptyDate={this.renderEmptyDate.bind(this)}
-            rowHasChanged={this.rowHasChanged.bind(this)}
-            theme={{ agendaKnobColor: 'grey' }}
+        <Agenda
+          items={this.state.doctorsAppointments}
+          selected={currentDate}
+          renderItem={this.renderItem.bind(this)}
+          renderEmptyDate={this.renderEmptyDate.bind(this)}
+          rowHasChanged={this.rowHasChanged.bind(this)}
+          theme={{ agendaKnobColor: 'grey' }}
 
-            // // the list of items that have to be displayed in agenda. If you want to render item as empty date
-            // // the value of date key kas to be an empty array []. If there exists no value for date key it is
-            // // considered that the date in question is not yet loaded
-            items={
-              {
-                '2019-01-28': [{ text: 'item 1 - any js object' }],
-                '2019-01-29': [{ text: 'item 2 - any js object' }],
-                '2019-01-30': [],
-                '2019-02-01': [{ text: 'item 3 - any js object' }, { text: 'any js object' }],
-              }}
-            // // callback that gets called when items for a certain month should be loaded (month became visible)
-            // loadItemsForMonth={(month) => { console.log('trigger items loading') }}
-            // // callback that fires when the calendar is opened or closed
-            // onCalendarToggled={(calendarOpened) => { console.log(calendarOpened) }}
-            // // callback that gets called on day press
-            // onDayPress={(day) => { console.log('day pressed') }}
-            // // callback that gets called when day changes while scrolling agenda list
-            // onDayChange={(day) => { console.log('day changed') }}
-            // // initially selected day
-            // selected={currentDate}
-            // // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-            // // minDate={yesterdayDate}
-            // // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-            // maxDate={nextMonthDate}
-            // // Max amount of months allowed to scroll to the past. Default = 50
-            // pastScrollRange={50}
-            // // Max amount of months allowed to scroll to the future. Default = 50
-            // futureScrollRange={50}
-            // // specify how each item should be rendered in agenda
-            // renderItem={(item, firstItemInDay) => { return (<View />); }}
-            // // specify how each date should be rendered. day can be undefined if the item is not first in that day.
-            // renderDay={(day, item) => { return (<View />); }}
-            // // specify how empty date content with no items should be rendered
-            // renderEmptyDate={() => { return (<View />); }}
-            // // specify how agenda knob should look like
-            // renderKnob={() => { return (<View />); }}
-            // // specify what should be rendered instead of ActivityIndicator
-            // renderEmptyData={() => { return (<View />); }}
-            // // specify your item comparison function for increased performance
-            // rowHasChanged={(r1, r2) => { return r1.text !== r2.text }}
-            // // If provided, a standard RefreshControl will be added for "Pull to Refresh" functionality. Make sure to also set the refreshing prop correctly.
-            // onRefresh={() => console.log('refreshing...')}
-            // // Set this true while waiting for new data from a refresh
-            // refreshing={false}
-            // // Add a custom RefreshControl component, used to provide pull-to-refresh functionality for the ScrollView.
-            // refreshControl={null}
+        // // the list of items that have to be displayed in agenda. If you want to render item as empty date
+        // // the value of date key kas to be an empty array []. If there exists no value for date key it is
+        // // considered that the date in question is not yet loaded
+        // items={
+        //   {
+        //     '2019-01-28': [{ name: 'item 1 - any js object' }, { name: 'item 2 - any js object' }],
+        //     '2019-01-29': [{ name: 'item 2 - any js object' }],
+        //     '2019-01-30': [],
+        //     '2019-02-01': [{ name: 'item 3 - any js object' }, { name: 'any js object' }],
+        //   }}
+        // // callback that gets called when items for a certain month should be loaded (month became visible)
+        // loadItemsForMonth={(month) => { console.log('trigger items loading') }}
+        // // callback that fires when the calendar is opened or closed
+        // onCalendarToggled={(calendarOpened) => { console.log(calendarOpened) }}
+        // // callback that gets called on day press
+        // onDayPress={(day) => { console.log('day pressed') }}
+        // // callback that gets called when day changes while scrolling agenda list
+        // onDayChange={(day) => { console.log('day changed') }}
+        // // initially selected day
+        // selected={currentDate}
+        // // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+        // // minDate={yesterdayDate}
+        // // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+        // maxDate={nextMonthDate}
+        // // Max amount of months allowed to scroll to the past. Default = 50
+        // pastScrollRange={50}
+        // // Max amount of months allowed to scroll to the future. Default = 50
+        // futureScrollRange={50}
+        // // specify how each item should be rendered in agenda
+        // renderItem={(item, firstItemInDay) => { return (<View />); }}
+        // // specify how each date should be rendered. day can be undefined if the item is not first in that day.
+        // renderDay={(day, item) => { return (<View />); }}
+        // // specify how empty date content with no items should be rendered
+        // renderEmptyDate={() => { return (<View />); }}
+        // // specify how agenda knob should look like
+        // renderKnob={() => { return (<View />); }}
+        // // specify what should be rendered instead of ActivityIndicator
+        // renderEmptyData={() => { return (<View />); }}
+        // // specify your item comparison function for increased performance
+        // rowHasChanged={(r1, r2) => { return r1.text !== r2.text }}
+        // // If provided, a standard RefreshControl will be added for "Pull to Refresh" functionality. Make sure to also set the refreshing prop correctly.
+        // onRefresh={() => console.log('refreshing...')}
+        // // Set this true while waiting for new data from a refresh
+        // refreshing={false}
+        // // Add a custom RefreshControl component, used to provide pull-to-refresh functionality for the ScrollView.
+        // refreshControl={null}
         />
         { //Check if state is looking for appointment
           (this.state.isLookingForAppointment)
@@ -149,7 +137,7 @@ export default class RequestsPage extends Component {
 
             </View>
         }
-          {/* <Calendar
+        {/* <Calendar
             // Initially visible month. Default = Date()
             current={currentDate}
             // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
@@ -202,31 +190,31 @@ export default class RequestsPage extends Component {
   } // End of render
 
 
-  loadItems(day) {
-    setTimeout(() => {
-      for (let i = -15; i < 85; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = this.timeToString(time);
-        if (!this.state.items[strTime]) {
-          this.state.items[strTime] = [];
-          const numItems = Math.floor(Math.random() * 5);
-          for (let j = 0; j < numItems; j++) {
-            this.state.items[strTime].push({
-              name: 'Item for ' + strTime,
-              height: Math.max(50, Math.floor(Math.random() * 150))
-            });
-          }
-        }
-      }
-      //console.log(this.state.items);
-      const newItems = {};
-      Object.keys(this.state.items).forEach(key => { newItems[key] = this.state.items[key]; });
-      this.setState({
-        items: newItems
-      });
-    }, 1000);
-    // console.log(`Load Items for ${day.year}-${day.month}`);
-  }
+  // loadItems(day) {
+  //   setTimeout(() => {
+  //     for (let i = -15; i < 85; i++) {
+  //       const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+  //       const strTime = this.timeToString(time);
+  //       if (!this.state.items[strTime]) {
+  //         this.state.items[strTime] = [];
+  //         const numItems = Math.floor(Math.random() * 5);
+  //         for (let j = 0; j < numItems; j++) {
+  //           this.state.items[strTime].push({
+  //             name: 'Item for ' + strTime,
+  //             height: Math.max(50, Math.floor(Math.random() * 150))
+  //           });
+  //         }
+  //       }
+  //     }
+  //     //console.log(this.state.items);
+  //     const newItems = {};
+  //     Object.keys(this.state.items).forEach(key => { newItems[key] = this.state.items[key]; });
+  //     this.setState({
+  //       items: newItems
+  //     })
+  //   }, 1000)
+  //   // console.log(`Load Items for ${day.year}-${day.month}`);
+  // }
 
   renderItem(item) {
     return (
@@ -236,7 +224,7 @@ export default class RequestsPage extends Component {
 
   renderEmptyDate() {
     return (
-      <View style={styles.emptyDate}><Text>No Events Today!</Text><Button onPress={() => this.requestAppointment()} title="Create New Request"/></View>
+      <View style={styles.emptyDate}><Text>No Events Today!</Text><Button onPress={() => this.requestAppointment()} title="Create New Request" /></View>
     );
   }
 
@@ -254,6 +242,9 @@ export default class RequestsPage extends Component {
 const height = Dimensions.get('window').height
 const width = Dimensions.get('window').width
 
+
+//  const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+//  const strTime = this.timeToString(time);
 let currentDate = new Date()
 let yesterdayDate = new Date().setDate(currentDate.getDate() - 1)
 let tomorrowDate = new Date().setDate(currentDate.getDate() + 1)
@@ -261,6 +252,7 @@ let nextMonthDate = new Date().setMonth(currentDate.getMonth() + 1)
 const vacation = { key: 'vacation', color: 'blue', selectedDotColor: 'blue' };
 const massage = { key: 'massage', color: 'orange', selectedDotColor: 'orange' };
 const workout = { key: 'workout', color: 'red' };
+
 const htmlContent = `
       <div id="bookingjs"></div>
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" defer></script >
